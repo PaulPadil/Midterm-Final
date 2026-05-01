37484b1d28fefa85fadbcb0085994b4a9845d9bc
RusselCode <russelarcega7@gmail.com>
2026-05-01 18:15:55 +0800

    KINULAYAN

--- MainS.py
+++ MainS.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # main.py
 
 from player import use_item, unequip_item, sell_item, show_inventory, show_stats
@@ -47,4 +48,55 @@ def main_menu():
 
 
 load_game()
+=======
+# main.py
+
+from player import use_item, unequip_item, sell_item, show_inventory, show_stats
+from shop import weapon_shop, armour_shop
+from save_load import load_game, save_game 
+
+
+def main_menu():
+    while True:
+        print("\n=== MENU ===")
+        print("1. Weapon Shop")
+        print("2. Armour Shop")
+        print("3. Inventory")
+        print("4. Stats")
+        print("5. Equip")
+        print("6. Unequip")
+        print("7. Sell item")
+        print("8. Exit")
+
+        choice = input("Choice: ")
+
+        if choice == "1":
+            weapon_shop()
+
+        elif choice == "2":
+            armour_shop()
+
+        elif choice == "3":
+            show_inventory()
+
+        elif choice == "4":
+            show_stats()
+
+        elif choice == "5":
+            use_item()
+
+        elif choice == "6":
+            unequip_item()
+
+        elif choice == "7":
+            sell_item()
+
+        elif choice == "8":
+            save_game()
+
+            break
+
+
+load_game()
+>>>>>>> c6675ae (TESTING NI RUSSEL)
 main_menu()
--- save_load.py
+++ save_load.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # save_load.py
 
 import json
@@ -29,3 +30,36 @@ def load_game():
     except FileNotFoundError:
         print("No save file found. Starting new game.")
 
+=======
+# save_load.py
+
+import json
+import player
+
+
+def save_game():
+    data = {
+        "gold": player.gold,
+        "inventory": player.inventory,
+        "equipped_weapon": player.equipped_weapon,
+        "equipped_armour": player.equipped_armour
+    }
+
+    with open("save_data.json", "w") as file:
+        json.dump(data, file)
+
+
+def load_game():
+    try:
+        with open("save_data.json", "r") as file:
+            data = json.load(file)
+
+            player.gold = data["gold"]
+            player.inventory = data["inventory"]
+            player.equipped_weapon = data["equipped_weapon"]
+            player.equipped_armour = data["equipped_armour"]
+
+    except FileNotFoundError:
+        print("No save file found. Starting new game.")
+
+>>>>>>> c6675ae (TESTING NI RUSSEL)

3f1a735719df0e9184fbbc106c70d0db03fc7f4f
RusselCode <russelarcega7@gmail.com>
2026-05-01 15:52:57 +0800

    NAKULAYAN KO NATO

--- MainS.py
+++ MainS.py
@@ -58,17 +58,17 @@ from save_load import load_game, save_game
 
 def main_menu():
     while True:
-        print("\n=== MENU ===")
-        print("1. Weapon Shop")
-        print("2. Armour Shop")
-        print("3. Inventory")
-        print("4. Stats")
-        print("5. Equip")
-        print("6. Unequip")
-        print("7. Sell item")
-        print("8. Exit")
+        print("\033[33m=== MENU ===\033[0m\n")
+        print("\033[32m1. Weapon Shop\033[0m")
+        print("\033[32m2. Armour Shop\033[0m")
+        print("\033[32m3. Inventory\033[0m")
+        print("\033[32m4. Stats\033[0m")
+        print("\033[32m5. Equip\033[0m")
+        print("\033[32m6. Unequip\033[0m")
+        print("\033[32m7. Sell item\033[0m")
+        print("\033[31m8. Exit\033[0m\n")
 
-        choice = input("Choice: ")
+        choice = input("\033[33mChoice: \033[0m")
 
         if choice == "1":
             weapon_shop()
